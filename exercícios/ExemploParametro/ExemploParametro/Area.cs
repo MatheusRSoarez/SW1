2b8bcae8abc73856cd37adfd13e79d72a1208623
Matheus Soares <124801798+MatheusRSoarez@users.noreply.github.com>
2024-04-05 12:45:59 -0300

    Add files via upload

---
+++ exercícios/ExemploParametro/ExemploParametro/Area.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExemploParametro
+{
+    internal class Area
+    {
+        public int Base { get; set; }
+        public int Altura { get; set; }
+        public float AreaCalculada { get; set; }  
+
+        public float calculaQuadrado()
+        {
+            if (Base == Altura)
+            {
+                AreaCalculada = Base * Altura;
+            }
+            else
+            {
+                Console.Write("Não é um quadrado. ");
+            }
+            return AreaCalculada;
+        }
+
+        public float calculaRetangulo()
+        {
+            AreaCalculada = Base * Altura;
+            return AreaCalculada;
+        }
+
+        public float calculaTriangulo()
+        {
+            float areaTriangulo = AreaCalculada = (Base * Altura)/2;
+            return areaTriangulo;
+        }
+
+        public void escolha(int opcao)
+        {
+           if(opcao == 1)
+            {
+                float quadrado = calculaQuadrado();
+                Console.WriteLine("Área do quadrado é: " + quadrado);
+            }else if(opcao == 2)
+            {
+                float retangulo = calculaRetangulo();
+                Console.WriteLine("Área do Retangulo é: " + retangulo);
+            }else if (opcao == 3)
+            {
+                float triangulo = calculaTriangulo();
+                Console.WriteLine("Área do triangulo é: " + triangulo);
+            }
+        }
+    }
+}
---
+++ exercícios/areaQuadradoRetangulo/areaQuadradoRetangulo/Area.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace areaQuadradoRetangulo
+{
+    public class Area
+    {
+        public int Base { get; set; }
+        public int Altura { get; set; }
+        public int AreaCalculada { get; set; }
+        public int Calcular()
+        {
+            AreaCalculada = Base * Altura;
+            return AreaCalculada;
+        }
+        public string Calcular2()
+        {
+            string resultado = "";
+            AreaCalculada = Base * Altura;
+            if (Base == Altura)
+            {
+                 resultado = "Quadrado: "+AreaCalculada;
+            }
+            else
+            {
+                resultado = "Retângulo: "+AreaCalculada;
+            }
+
+            return resultado;
+        }
+
+        public void MostraDetalhes()
+        {
+            Console.WriteLine("Base: "+Base);
+            Console.WriteLine("Altura: "+Altura);
+            Console.WriteLine("Area: " + AreaCalculada);
+        }
+    }
+}
